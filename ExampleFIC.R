e9a83ea95a12a5cf612b383d1e33cf34becd8af9
Francisco Javier Rubio <33929387+FJRubio67@users.noreply.github.com>
2024-07-06 13:30:16 +0100

    routines and example

---
+++ ExampleFIC.R
@@ -0,0 +1,233 @@
+#######################################################################################
+# Data preparation
+#######################################################################################
+
+rm(list=ls())
+
+# Required packages
+library(deSolve)
+library(survival)
+library(ggplot2)
+#library(devtools)
+#install_github("FJRubio67/HazReg")
+library(HazReg)
+library(Rtwalk)
+library(knitr)
+
+
+## ----include=FALSE----------------------------------------------------------------------------------------
+
+#source("/Users/FJRubio/Dropbox/ODESurv/HO/codes/routines/routinesHO.R")
+#source("C:/Users/Javier/Dropbox/ODESurv/HO/codes/routines/routinesHO.R")
+source("/Users/javierrubio/Dropbox/ODESurv/HO/codes/routines/routinesHO.R")
+
+## ----eval=FALSE-------------------------------------------------------------------------------------------
+## source("routines.R")
+
+
+
+## -----------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+# Data preparation
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+
+
+head(rotterdam)
+
+dim(rotterdam)
+
+# Kaplan-Meier estimator for the survival times
+km <- survfit(Surv(rotterdam$dtime/365.24, rotterdam$death) ~ 1)
+
+plot(km$time, km$surv, type = "l", col = "black", lwd = 2, lty = 1, 
+     ylim = c(0,1), xlab = "Time", ylab = "Survival")
+
+
+
+# New data frame: logical status, time in years, survival times sorted
+df <- data.frame(time = rotterdam$dtime, status = rotterdam$death)
+df$status <- as.logical(rotterdam$death)
+df$time <- df$time/365.24
+
+df <- df[order(df$time),]
+
+# Required quantities
+status <- as.logical(df$status)
+t_obs <- df$time[status]
+survtimes <- df$time
+
+
+
+
+
+## ---------------------------------------------------------------------------------------------------------
+#==================================================================================================
+# MLE Analysis
+#==================================================================================================
+
+#--------------------------------------------------------------------------------------------------
+# Harmonic Oscillator ODE model for the hazard function: Analytic solution
+#--------------------------------------------------------------------------------------------------
+
+# Initial point
+initHO <- c(0,0,0)
+
+# Initial conditions
+
+St <- 1-0.05/12
+Stt <- 1 - 2*0.05/12
+S0 <- 1
+dt <- 1/12
+
+Sp <- -(St-S0)/dt
+
+h0s <- Sp/St
+
+h0s
+
+Spp <- (Stt - 2*St + S0)/(dt^2)
+h0p <- h0s^2 - Spp/St
+
+h0p
+
+h00 <- h0s
+r00 <- h0p
+
+
+nlminb(c(.5,0.1,0.1), log_likHOFICS, control = list(iter.max = 1000))
+
+## ---------------------------------------------------------------------------------------------------------
+#==================================================================================================
+# Bayesian Analysis
+#==================================================================================================
+
+
+#--------------------------------------------------------------------------------------------------
+# Harmonic Oscillator ODE model for the hazard function: Analytic solution
+#--------------------------------------------------------------------------------------------------
+
+
+# Random initial points
+X0HO <- function(x) { initHO + runif(3,-0.05,0.05) }
+
+NMC = 60000
+
+# twalk for analytic solution
+set.seed(1234)
+infoHO <- Runtwalk( dim=3,  Tr=NMC,  Obj=log_postHOFIC, Supp=SupportHOFIC, 
+                    x0=X0HO(), xp0=X0HO(),PlotLogPost = TRUE) 
+
+ind = ind=seq(10000,NMC,100) 
+
+
+postHO <- cbind(exp(infoHO$output[ind,1:3]))
+
+taup <- as.vector(postHO[,1])
+w2p <- as.vector(postHO[,2])
+hbp <- as.vector(postHO[,3])
+
+Ap <- vector()
+phip <- vector()
+
+for(i in 1:length(ind)) {
+  newpar <- hr2ap(taup[i], w2p[i], hbp[i], h00, r00)
+  
+  Ap[i] <- as.numeric(newpar$A)
+  phip[i] <- as.numeric(newpar$phi)
+}
+
+
+hist(taup)
+hist(w2p)
+hist(hbp)
+hist(Ap)
+hist(phip)
+
+
+# Predictive HO hazard
+predhHO <- Vectorize(function(t){
+  num <- den <- temp <- vector()
+  for(i in 1:length(ind)){
+    chi <- chshoode( t, taup[i], w2p[i], hbp[i], Ap[i], phip[i] )
+    num[i] <- exp(-chi)*hshoode( t, taup[i], w2p[i], hbp[i], Ap[i], phip[i] )
+    den[i] <- exp(-chi)
+  }
+  return(mean(num)/mean(den))
+})
+
+
+# Creating the credible envelopes
+tvec <- seq(0,20,by = 0.01)
+ntvec <- length(tvec)
+
+hHOs <-  predhHO(tvec)
+
+hCIHO<- matrix(0, ncol = ntvec, nrow = length(ind))
+
+for(j in 1:length(ind)){
+  for(k in 1:ntvec){
+    hCIHO[j,k ] <- hshoode( tvec[k],taup[j], w2p[j], hbp[j], Ap[j], phip[j]) 
+  }
+} 
+
+hCIHOL <- apply(hCIHO, 2, ql)
+hCIHOU <- apply(hCIHO, 2, qu)
+fit <- bshazard(Surv(df$time, df$status) ~ 1, data = df, nbin = 100, degree = 3, verbose = FALSE)
+
+curve(predhHO, 0, 20, n = 1000, xlab = "Time", ylab = "Predictive Hazard", 
+      cex.axis = 1.5, cex.lab = 1.5, lwd =2, lty = 1, ylim = c(0,0.065))
+
+
+plot(tvec,  hHOs, type = "l", xlab = "Time", ylab = "Predictive Hazard", 
+       cex.axis = 1.5, cex.lab = 1.5, lwd =2, lty = 1, ylim = c(0,0.1))
+points(tvec,  hCIHOL, col = "gray", type = "l")
+points(tvec,  hCIHOU, col = "gray", type = "l")
+polygon(c(tvec, rev(tvec)), c(hCIHOL[order(tvec)], rev(hCIHOU[order(tvec)])),
+        col = "gray", border = NA)
+points(fit$time, fit$hazard, type='l', lwd = 2, lty = 2, ylim = c(0,0.25), col = "darkgray")
+lines(fit$time, fit$lower.ci, lty = 2, lwd = 1, col = "gray")
+lines(fit$time, fit$upper.ci, lty = 2, lwd = 1, col = "gray")
+points(tvec,  hHOs, type = "l", xlab = "Time", ylab = "Predictive Hazard", 
+       cex.axis = 1.5, cex.lab = 1.5, lwd =2, lty = 1)
+legend("bottomright", legend = c("HO", "BS"), lty = c(1,2),  
+       lwd = c(2,2), col = c("black","darkgray")) 
+
+
+
+# Predictive HO survival 
+predSHO <- Vectorize(function(t){ 
+  temp <- vector() 
+  for(i in 1:length(ind)) temp[i] <- exp(-chshoode( t,taup[i], w2p[i], hbp[i], Ap[i], phip[i]) )  
+  return(mean(temp)) 
+}) 
+
+
+
+SCIHO <- matrix(0, ncol = ntvec, nrow = length(ind)) 
+
+for(j in 1:length(ind)){ 
+  for(k in 1:ntvec){ 
+    SCIHO[j,k ] <- exp(-chshoode( tvec[k],taup[j], w2p[j], hbp[j], Ap[j], phip[j])) 
+  } 
+}
+
+
+SpredHO <-  predSHO(tvec) 
+
+SCIHOL <- apply(SCIHO, 2, ql) 
+SCIHOU <- apply(SCIHO, 2, qu) 
+
+
+plot(tvec,  SpredHO, type = "l", ylim = c(0,1), xlab = "Time", ylab = "Predictive Survival",  
+       cex.axis = 1.5, cex.lab = 1.5, lwd =2, lty = 1) 
+points(tvec,  SCIHOL, col = "gray", type = "l") 
+points(tvec,  SCIHOU, col = "gray", type = "l") 
+polygon(c(tvec, rev(tvec)), c(SCIHOL[order(tvec)], rev(SCIHOU[order(tvec)])), 
+        col = "gray", border = NA) 
+points(tvec,  SpredHO,type = "l", col = "black", lwd = 2, lty =1) 
+points(km$time, km$surv, type = "l", col = "black", lwd = 2, lty = 2, 
+     ylim = c(0,1), xlab = "Time", ylab = "Survival")
+legend("topright", legend = c("HO", "KM"), lty = c(1,2),  
+       lwd = c(2,2), col = c("black","black")) 
---
+++ routinesHO.R
@@ -0,0 +1,504 @@
+# Quantile functions
+ql <- function(p) quantile(p, 0.025)
+qu <- function(p) quantile(p, 0.975)
+
+
+######################################################################################
+######################################################################################
+######################################################################################
+# Weibull
+######################################################################################
+######################################################################################
+######################################################################################
+
+
+#--------------------------------------------------------------------------------------------------
+# Weibull -log-posterior function
+#--------------------------------------------------------------------------------------------------
+
+log_postW <- function(par){
+  sigma <- exp(par[1]); nu <- exp(par[2]);
+  
+  # Terms in the log log likelihood function
+  ll_haz <- sum(hweibull(t_obs, sigma, nu, log = TRUE))
+  
+  ll_chaz <- sum(chweibull(survtimes, sigma, nu))
+  
+  log_lik <- -ll_haz + ll_chaz 
+  
+  # Log prior
+  
+  log_prior <- -dgamma(sigma, shape = 2, scale = 2, log = TRUE) - 
+    dgamma(nu, shape = 2, scale = 2, log = TRUE) 
+  
+  # Log Jacobian
+  
+  log_jacobian <- -par[1] - par[2] 
+  
+  # log posterior
+  
+  log_post0 <- log_lik + log_prior + log_jacobian
+  
+  return(as.numeric(log_post0))
+}
+
+######################################################################################
+######################################################################################
+######################################################################################
+# Power Generalised Weibull
+######################################################################################
+######################################################################################
+######################################################################################
+
+
+#--------------------------------------------------------------------------------------------------
+# Power Generalised Weibull -log-posterior function
+#--------------------------------------------------------------------------------------------------
+
+log_postPGW <- function(par){
+  sigma <- exp(par[1]); nu <- exp(par[2]); gamma <- exp(par[3])
+  
+  # Terms in the log log likelihood function
+  ll_haz <- sum(hpgw(t_obs, sigma, nu, gamma, log = TRUE))
+  
+  ll_chaz <- sum(chpgw(survtimes, sigma, nu, gamma))
+  
+  log_lik <- -ll_haz + ll_chaz 
+  
+  # Log prior
+  
+  log_prior <- -dgamma(sigma, shape = 2, scale = 2, log = TRUE) - 
+    dgamma(nu, shape = 2, scale = 2, log = TRUE) - 
+    dgamma(gamma, shape = 2, scale = 2, log = TRUE) 
+  
+  # Log Jacobian
+  
+  log_jacobian <- -par[1] - par[2] - par[3]
+  
+  # log posterior
+  
+  log_post0 <- log_lik + log_prior + log_jacobian
+  
+  return(as.numeric(log_post0))
+}
+
+
+######################################################################################
+######################################################################################
+######################################################################################
+# Harmonic Oscillator 
+######################################################################################
+######################################################################################
+######################################################################################
+
+
+#-----------------------------------------------------------------------------
+# Reparameterisations
+#-----------------------------------------------------------------------------
+
+
+# reparameterisation to A and phi
+rep2ap <- function(eta, w0, hb, h0, r0){
+  test <- r0 + w0*eta*(h0 - hb) 
+  testr <- h0 - hb
+  w1 <- w0*sqrt(abs(eta^2-1))
+  
+  if(testr == 0){
+    phi <- 0
+    A <- r0/w1
+  }
+  
+  if(test == 0){
+    phi <- sign(h0-hb)*pi/2
+    A <- h0 - hb
+  }
+  
+  if(test != 0 & testr != 0){
+    phi <- atan( ( w1*(h0-hb) )/( r0 + w0*eta*(h0 - hb) ) )
+    A <- (h0-hb)/sin(phi)
+  }
+  return(list(A = A, phi = phi))
+}
+
+
+#-----------------------------------------------------------------------------
+# Harmonic Oscillator ODE Hazard Function: Analytic solution
+#-----------------------------------------------------------------------------
+# t: time (positive)
+# eta: shape parameter (positive)
+# w0 : shape parameter (positive)
+# hb: shift parameter (positive)
+# h0 : hazard initial condition (positive)
+# r0 : hazard derivative initial condition (real)
+
+hHO <- function(t, eta, w0, hb, h0, r0){
+  # Notation
+  w1 <- w0*sqrt(abs(eta^2-1))
+  
+  # Under-damped
+  if(eta<1){
+    rep = rep2ap(eta, w0, hb, h0, r0)
+    A <- rep$A
+    phi <- rep$phi
+    haz <-  hb + A*exp(-w0*eta*t)*sin( w1*t + phi )
+  }
+  
+  # Over-damped
+  if(eta>1){
+    mu = w1/(w0*eta)
+    a = (h0 - hb)/mu + r0/w1 
+    haz <- hb + exp(-w0*eta*t)*( 0.5*(h0-hb+a)*exp(w1*t) +
+                                   0.5*(h0-hb-a)*exp(-w1*t))
+  }
+  
+  # Critically damped
+  if(eta==1){
+    haz <- hb + ( h0 - hb + t*(r0 + w0*(h0-hb)) )*exp(-w0*t)
+  }
+  return(haz)
+}
+
+
+#-----------------------------------------------------------------------------
+# Harmonic Oscillator ODE Cumulative Hazard Function: Analytic solution
+#-----------------------------------------------------------------------------
+# t: time (positive)
+# eta: shape parameter (positive)
+# w0 : shape parameter (positive)
+# hb: shift parameter (positive)
+# h0 : hazard initial condition (positive)
+# r0 : hazard derivative initial condition (real)
+
+chHO <- function(t, eta, w0, hb, h0, r0){
+  
+  # Notation
+  w1 <- w0*sqrt(abs(eta^2-1))
+  mu = w1/(w0*eta)
+  
+  # Under-damped
+  if(eta<1){
+    rep = rep2ap(eta, w0, hb, h0, r0)
+    A <- rep$A
+    phi <- rep$phi
+    chaz <- hb*t + 
+            (A/(w0*eta))*( sin(phi) + mu*cos(phi) -
+            exp(-w0*eta*t)*( sin(w1*t+phi) + mu*cos(w1*t+phi) )  )/(mu^2+1)
+  }
+  
+  # Over-damped
+  if(eta>1){
+
+    a = (h0 - hb)/mu + r0/w1 
+    
+    chaz <- hb*t + ( 0.5*(h0-hb+a)*(exp((w1 - w0*eta)*t)-1)/(w1-w0*eta) -
+                     0.5*(h0-hb-a)*(exp((-w1 - w0*eta)*t)-1)/(w1+w0*eta) )
+  }
+  
+  # Critically-damped
+  if(eta==1){
+    chaz <- hb*t + (h0-hb)*(1-exp(-w0*t))/w0 +
+            (r0+w0*(h0-hb))*(exp(w0*t)-w0*t-1)*exp(-w0*t)/(w0^2)
+  }
+  return(chaz)
+}
+
+#-----------------------------------------------------------------------------
+# Shifted Harmonic Oscillator ODE Probability Density Function: Analytic solution
+#-----------------------------------------------------------------------------
+# t: time (positive)
+# tau: shape parameter
+# w2 : shape parameter
+# hb: shift parameter
+# A: Amplitude 
+# phi: Phase
+dHO  <- function(t, eta, w0, hb, h0, r0){
+  den <-  hHO(t, eta, w0, hb, h0, r0)*
+    exp(-chHO(t, eta, w0, hb, h0, r0))
+    return(den)
+}
+
+#-----------------------------------------------------------------------------
+# Shifted Harmonic Oscillator ODE random number generation: Analytic solution
+#-----------------------------------------------------------------------------
+# n: number of simulations
+# tau: shape parameter
+# w2 : shape parameter
+# hb: shift parameter
+# A: Amplitude 
+# phi: Phase
+# interval: interval to find the roots
+
+rHO <- function(n, interval, eta, w0, hb, h0, r0){
+  u <- runif(n)
+  out <- vector()
+  
+  for(i in 1:n){
+    tempf <- Vectorize(function(t) chHO(t, eta, w0, hb, h0, r0) + log(u[i]) )
+    out[i] <- uniroot(tempf, interval = interval)$root
+  }
+  
+  return(as.vector(out))
+}
+
+
+
+
+######################################################################################################
+# Function to plot the hazard function
+######################################################################################################
+# par: parameters
+# xl: lower limit for plot - x axis
+# xu: upper limit for plot - y axis
+# yl: lower limit for plot - y axis
+# yu: upper limit for plot - y axis
+# param: parametrization. "original" = c(tau, w2, hb, h0, r0), repar = c(tau, w2, hb, A, phi)
+# n: number of points
+
+plotSHOhaz <- function(par, xl, xu, yl, yu, add.plot = FALSE, col = "black", lty = 1, lwd = 2,
+                       param = "original", n = 100){
+  if(param == "original"){
+    haz <- Vectorize( function(t) hHO(t, par[1], par[2], par[3], par[4], par[5]) )
+  }
+  if(param == "repar"){
+  reparam <- rep2ap(par[1], par[2], par[3], par[4], par[5])
+  A <- reparam$A
+  phi <- reparam$phi
+  haz <- Vectorize( function(t) hHO(t, par[1], par[2], par[3], A, phi) )
+}
+  curve(haz,xl,xu, ylim = c(yl,yu),  col = col, cex.axis = 1.25, cex.lab = 1.25,
+        ylab = "h(t)", xlab = "t", lty = lty, lwd = lwd, add = add.plot, n = n)
+}
+
+
+
+
+######################################################################################
+######################################################################################
+######################################################################################
+# Shifted Harmonic Oscillator
+######################################################################################
+######################################################################################
+######################################################################################
+
+
+# Support function on the log scale
+SupportHO <- function(par){
+  eta <- exp(par[1]); w0 <- exp(par[2]); hb <- exp(par[3]); 
+  h0 <- exp(par[4]); r0 <- par[5] 
+  
+  w1 <- w0*sqrt(abs(eta^2-1))
+  mu <- w1/(w0*eta)
+  phi <- rep2ap(eta, w0, hb, h0, r0)$phi
+  
+  temph <- Vectorize(function(t) hHO(t, eta, w0, hb, h0, r0))
+  
+  if(eta < 1){
+  ts <- (atan(mu) - phi + seq(0, 1, by = 1)*pi)/w1
+
+  
+  hs <- temph(ts)
+  
+  out <- ifelse(any(hs<=0), FALSE, TRUE)
+  }
+  
+  if(eta > 1){
+    if(r0 >= 0) out <- TRUE
+    if(r0 < 0){
+      test <- (h0-hb-a)/(h0-hb+a)
+      if(test <= 0) out = FALSE
+      if(test > 0){
+        t_hat <- 0.5*w0*eta*log(test)/mu
+
+        hs <- temph(c(0.5*t_hat, t_hat))
+        
+        out <- ifelse(any(hs<=0), FALSE, TRUE)
+      }
+      }
+
+  }
+  if(eta == 1) out <- FALSE
+  
+  return(out)
+  
+}
+
+
+# Support function on the log scale
+# Fixed initial conditions
+SupportHOFICS <- function(par){
+  eta <- exp(par[1]); w0 <- exp(par[2]); hb <- exp(par[3]); 
+  h0 <- h00; r0 <- r00 
+  
+  w1 <- w0*sqrt(abs(eta^2-1))
+  mu <- w1/(w0*eta)
+  phi <- rep2ap(eta, w0, hb, h0, r0)$phi
+  
+  temph <- Vectorize(function(t) hHO(t, eta, w0, hb, h0, r0))
+  
+  if(eta < 1){
+    ts <- (atan(mu) - phi + seq(0, 1, by = 1)*pi)/w1
+    
+    
+    hs <- temph(ts)
+    
+    out <- ifelse(any(hs<=0), FALSE, TRUE)
+  }
+  
+  if(eta > 1){
+    if(r0 >= 0) out <- TRUE
+    if(r0 < 0){
+      test <- (h0-hb-a)/(h0-hb+a)
+      if(test <= 0) out = FALSE
+      if(test > 0){
+        t_hat <- 0.5*w0*eta*log(test)/mu
+        
+        hs <- temph(c(0.5*t_hat, t_hat))
+        
+        out <- ifelse(any(hs<=0), FALSE, TRUE)
+      }
+    }
+    
+  }
+  if(eta == 1) out <- FALSE
+  
+  return(out)
+  
+}
+
+#--------------------------------------------------------------------------------------------------
+# Shifted Harmonic Oscillator ODE -log-likelihood function: Analytic solution
+#--------------------------------------------------------------------------------------------------
+
+log_likHO <- function(par){
+  tau <- exp(par[1]); w2 <- exp(par[2]); hb <- exp(par[3]); 
+  h0 <- exp(par[4]); r0 <- par[5] 
+  
+  newpar <- reo2ap(tau, w2, hb, h0, r0)
+  
+  A <- as.numeric(newpar$A)
+  phi <- as.numeric(newpar$phi)
+  
+  cond <- SupportHO(par)
+  
+  if(!cond) log_lik <- Inf
+  
+  if(cond){
+    # Terms in the log log likelihood function
+    ll_haz <- sum(log(hHO(t_obs, tau, w2, hb, A, phi)))
+    
+    ll_chaz <- sum(chHO(survtimes, tau, w2, hb, A, phi ))
+    
+    log_lik <- -ll_haz + ll_chaz 
+  }
+  
+  return(log_lik)
+}
+
+
+
+#--------------------------------------------------------------------------------------------------
+# Shifted Harmonic Oscillator ODE -log-posterior function: Analytic solution
+#--------------------------------------------------------------------------------------------------
+
+log_postHO <- function(par){
+  tau <- exp(par[1]); w2 <- exp(par[2]); hb <- exp(par[3]); 
+  h0 <- exp(par[4]); r0 <- par[5] 
+  
+  newpar <- rep2ap(tau, w2, hb, h0, r0)
+  
+  A <- as.numeric(newpar$A)
+  phi <- as.numeric(newpar$phi)
+  
+  # Terms in the log log likelihood function
+  ll_haz <- sum(log(hHO(t_obs, tau, w2, hb, A, phi)))
+  
+  ll_chaz <- sum(chHO(survtimes, tau, w2, hb, A, phi ))
+  
+  log_lik <- -ll_haz + ll_chaz 
+  
+  # Log prior
+  
+  
+  log_prior <- -dgamma(tau, shape = 2, scale = 2, log = TRUE) - 
+    dgamma(w2, shape = 2, scale = 2, log = TRUE) -
+    dgamma(hb, shape = 2, scale = 2, log = TRUE) -
+    dgamma(h0, shape = 2, scale = 2, log = TRUE) -
+    dnorm(r0, mean = 0, sd = 100, log = TRUE)
+  
+  # Log-Jacobian
+  
+  log_jacobian <- -log(tau) - log(w2) - log(hb) - log(h0) 
+  
+  # log posterior
+  
+  log_post0 <- log_lik + log_prior + log_jacobian
+  
+  return(as.numeric(log_post0))
+}
+
+
+
+#--------------------------------------------------------------------------------------------------
+# Shifted Harmonic Oscillator ODE -log-likelihood function: Analytic solution
+# Fixed initial conditions
+#--------------------------------------------------------------------------------------------------
+
+log_likHOFICS <- function(par){
+  eta <- exp(par[1]); w0 <- exp(par[2]); hb <- exp(par[3]); 
+  
+  cond <- SupportHOFICS(par)
+  
+  if(!cond) log_lik <- Inf
+  
+  if(cond){
+    # Terms in the log log likelihood function
+    ll_haz <- sum(log(hHO(t_obs, eta, w0, hb, h00, r00)))
+    
+    ll_chaz <- sum(chHO(survtimes, eta, w0, hb, h00, r00 ))
+    
+    log_lik <- -ll_haz + ll_chaz 
+  }
+  
+  return(log_lik)
+}
+
+
+
+#--------------------------------------------------------------------------------------------------
+# Shifted Harmonic Oscillator ODE -log-posterior function: Analytic solution
+# Fixed initial conditions
+#--------------------------------------------------------------------------------------------------
+
+log_postHOFIC <- function(par){
+  tau <- exp(par[1]); w2 <- exp(par[2]); hb <- exp(par[3]); 
+  
+  newpar <- rep2ap(tau, w2, hb, h00, r00)
+  
+  A <- as.numeric(newpar$A)
+  phi <- as.numeric(newpar$phi)
+  
+  # Terms in the log log likelihood function
+  ll_haz <- sum(log(hHO(t_obs, tau, w2, hb, A, phi)))
+  
+  ll_chaz <- sum(chHO(survtimes, tau, w2, hb, A, phi ))
+  
+  log_lik <- -ll_haz + ll_chaz 
+  
+  # Log prior
+  
+  
+  log_prior <- -dgamma(tau, shape = 2, scale = 2, log = TRUE) - 
+    dgamma(w2, shape = 2, scale = 2, log = TRUE) -
+    dgamma(hb, shape = 2, scale = 2, log = TRUE) 
+  
+  # Log-Jacobian
+  
+  log_jacobian <- -log(tau) - log(w2) - log(hb) 
+  
+  # log posterior
+  
+  log_post0 <- log_lik + log_prior + log_jacobian
+  
+  return(as.numeric(log_post0))
+}
+
